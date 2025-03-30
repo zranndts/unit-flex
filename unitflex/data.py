26d8e132bd3358c423f0303d7052ceab2834ba5f
rannidts_ <vleaxorzz@gmail.com>
2025-03-30 21:51:56 +0700

    data.py

---
+++ unitflex/data.py
@@ -0,0 +1,41 @@
+class dataConverter:
+    conversionRates = {
+        "mb": 1,
+        "bit": 0.000000125,
+        "byte": 0.000001,
+        "kb": 0.001,
+        "gigabyte": 1000,
+        "terabyte":1000000,
+    }
+
+    @classmethod
+    def convert(cls, value, fromUnit, toUnit, precision=1, format="compact"):
+        fromUnit = fromUnit.lower()
+        toUnit = toUnit.lower()
+
+        if fromUnit not in cls.conversionRates:
+            raise ValueError(f"From unit '{fromUnit}' not recognized!")
+        if toUnit not in cls.conversionRates:
+            raise ValueError(f"To unit '{toUnit}' not recognized!")
+
+        defaultValue = value * cls.conversionRates[fromUnit]
+        convertedValue = defaultValue / cls.conversionRates[toUnit]
+
+        if int(precision) < 0:
+            raise ValueError("precisson can't be negative!")
+        
+        validationFormatting = {"raw", "compact", "verbose"}
+        if format not in validationFormatting:
+            raise (f"Output format '{format}' not recognized! Choose from: {', '.join(validationFormatting)}")
+    
+        if format == "raw":
+            result = f"{round(convertedValue, int(precision))}"
+            return result
+        elif format == "compact":
+            result = f"{round(convertedValue, int(precision))} {toUnit}"
+            return result
+        elif format == "verbose":
+            result = f"{value} {fromUnit} = {round(convertedValue, int(precision))} {toUnit}"
+            return result
+        else:
+            raise ValueError("Unexpected format parameters!")
