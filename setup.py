75577240c44fb417e827bcdf6abc5aba8cb2c1d4
Zahraan Dzakii <vleaxorzz@gmail.com>
2025-04-02 04:29:21 +0700

    feat: add setup.py and fix pyproject.toml for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unitflex",
+    version="0.1.0",  
+    author="Zahraan",
+    author_email="vleaxorzz@gmail.com",
+    description="Unit Converter Library",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zranndts/unitflex",
+    packages=find_packages(),  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6", 
+    install_requires=[], 
+)
