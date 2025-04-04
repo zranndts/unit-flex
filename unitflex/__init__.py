3e1b07b0ac5b1f411b4f0ba1fab87466be6dc3c4
Zahraan Dzakii <vleaxorzz@gmail.com>
2025-04-05 03:22:50 +0700

    feat(init): add shortcut imports for vol, press, speed, and time

--- unitflex/__init__.py
+++ unitflex/__init__.py
@@ -2,4 +2,7 @@ from .length import lengthConverter as length
 from .mass import massConverter as mass
 from .temperature import temperatureConverter as temper
 from .data import dataConverter as data
-
+from .volume import volumeConverter as vol
+from .pressure import pressureConverter as press
+from .speed import speedConverter as speed
+from .time import timeConverter as time
